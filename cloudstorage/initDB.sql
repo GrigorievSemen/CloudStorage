6cd04371242b21d55e625d3fc3eb74aaaabe4080
Grigoriev_Semen <byblik.sem@yandex.ru>
2023-05-01 16:16:17 +0300

    Make Docker Compose

--- cloudstorage/initDB.sql
+++ cloudstorage/initDB.sql
@@ -23,3 +23,12 @@ CREATE TABLE public.files
     FOREIGN KEY (holder) REFERENCES public.users (email) ON DELETE CASCADE
 );
 CREATE INDEX ON public.files (holder);
+
+DELETE
+FROM users;
+
+CREATE EXTENSION if not exists pgcrypto;
+
+INSERT INTO users (email, password)
+VALUES ('user1@yandex.com', crypt('test1', gen_salt('bf'))),
+       ('user2@yandex.com', crypt('test2', gen_salt('bf')));
